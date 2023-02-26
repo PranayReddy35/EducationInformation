d4cf9655ebb7fe712cc60e480b45a2df8a549037
PranayReddy35 <94630894+PranayReddy35@users.noreply.github.com>
2023-02-25 22:46:54 -0500

    Add files via upload

---
+++ edu-information-api/src/main/java/com/eduinformation/eduinformationapi/EduInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduinformation.eduinformationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduInformationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduInformationApiApplication.class, args);
+	}
+
+}
---
+++ edu-information-api/src/test/java/com/eduinformation/eduinformationapi/EduInformationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduinformation.eduinformationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduInformationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
